cfd5d47b6d57fce2eac8abbfad62eace1278ff5b
Jules Larue <jules.larue@protonmail.com>
2025-04-27 13:16:56 +0200

    Test

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Jules Larue';
+const name = 'Jules Larue - Branch';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home=false }) {
